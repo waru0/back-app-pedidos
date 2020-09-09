a234978a7cf99f544d1e48506fd2bf2531e341be
Waru0 <waru077@gmail.com>
2020-09-08 21:55:43 -0400

    controllers y routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 //IMPORTS
-const express = requiere("express");
+const express = require("express");
 const morgan = require("morgan");
 //importando el modulo de rutas
-const routes = require("./routes/index.js)");
+const routes = require("./routes/index.js");
 
 //CONFIGS
 const app = express();
 app.use(morgan("dev"));
 
 //HABILITAR RUTAS
-routes.rutas;
+routes.rutas(app);
 //LEVANTAR SERVICIO
 app.listen(3000, () => {
-  console.log("servidor levantado en: ");
+  console.log("Servidor levantado en: ");
 });
