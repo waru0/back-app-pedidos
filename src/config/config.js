14102fc3e9970340c43e04e12761b9156cbde794
Waru0 <waru077@gmail.com>
2020-09-16 21:16:39 -0400

    login

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
   port: 3000,
+  JWT_SECRET: "MICODIGOSECRETO",
+  JWT_EXPIRE: "1h",
 };
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,38 @@
-const usuario = require("./../models/Usuario");
 const Usuario = require("./../models/Usuario");
+const config = require("./../config/config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./../config/config");
 const login = async (req, res) => {
   //1. Verificar si el email esta registrado
 
-  const user = await Usuario.findOne({ email: req.body.email });
+  const user = await Usuario.findOne({ correo: req.body.correo });
   if (!user) {
     res.json({ mensaje: "Email inexistente" });
   } else {
+    const estado = await bcrypt.compare(req.body.password, user.password);
+    if (estado) {
+      //generar token(JWT)
+      const payload = {
+        correo: user.correo,
+        id: user._id,
+        time: new Date(),
+      };
+      var token = jwt.sign(payload, config.JWT_SECRET, {
+        expiresIn: config.JWT_EXPIRE,
+      });
+      res.json({
+        access_token: token,
+        usuario: {
+          _id: user._id,
+          usuario: user.usuario,
+          correo: user.email,
+          fecha: new Date(),
+        },
+      });
+    } else {
+      res.json({ mensaje: "contraseña incorrecta" });
+    }
     res.json({ mensaje: "Bienvenido" + req.body.email });
   }
   //2.
